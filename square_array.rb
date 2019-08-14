3465f1c809b4f166df3e410be0eed1f8c69af520
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-14 19:20:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
+#def square_array(array)
+#  new_array = []
+#  array.each do |element|
+#    new_array << element**2 
+#  end 
+#  return new_array
+#end
+
 def square_array(array)
-  new_array = []
-  array.each do |element|
-    new_array << element**2 
+  array.collect do |element|
+    element**2 
   end 
-  return new_array
-end
+end 
